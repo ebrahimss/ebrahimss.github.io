081e9ccca8c2819ea62bdb1a4f8d2c7f6330ed2a
Ebrahim Sharifnia <ssharifn@vols.utk.edu>
2024-04-10 23:07:39 -0400

    edit sidebar

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -16,23 +16,26 @@ const SideBar = () => (
         <h2>Ebrahim Sharifnia</h2>
         <p><a href="mailto:ebrahim.sharif.n@gmail.com">ebrahim.sharif.n@gmail.com</a></p>
       </header>
-    </section>
-
-    <section className="blurb">
-      <h2>About</h2>
-      <p>Hi, I&apos;m Ebrahim. I am a PhD candidate at <a href="https://ise.utk.edu//">Uni of Tennessee ISE</a>, and a <a href="https://en.sharif.edu///">Sharif</a> Alumni.
+      <p>Greetings! I&apos;m Ebrahim Sharifnia, currently advancing my doctoral studies at the <a href="https://ise.utk.edu//">University of Tennessee&apos;s Department of Industrial & Systems Engineering</a> an a concurretn masters degree in statistics at the <a href="https://haslam.utk.edu/</p>">Haslam College of Business</a>, and an academic foundation from <a href="https://en.sharif.edu///">Sharif University of Technology</a>.
       </p>
       <ul className="actions">
         <li>
           {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
         </li>
       </ul>
+      <ContactIcons />
+      {/* <p className="copyright">&copy;}
+      { Ebrahim Sharifnia <Link to="/">ebrahim.sharif.n@gmail.com</Link>.</p> */}
     </section>
 
-    <section id="footer">
-      <ContactIcons />
-      <p className="copyright">&copy; Ebrahim &apos;Sharifnia <Link to="/">ebrahim.sharif.n@gmail.com</Link>.</p>
+    {/* <section id="footer">
+
     </section>
+
+    <section className="blurb" style={{ width: '400px', margin: '0 auto', padding: '10px' }}>
+
+    </section> */}
+
   </section>
 );
 
